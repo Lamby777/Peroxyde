39ef7995d7c81215f9c3680c2a5571366c202767
Dexie <sheepareawesome777@gmail.com>
2020-01-17 16:49:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const Express = require("express");
+const request = require("request");
+const stream = require("stream");
+const ejs = require("ejs");
+const app = new Express();
+const port = 3040;
+let timesUsed = 0;
+let lastReset = new Date();
+
+app.set("view engine", "ejs");
+app.use(Express.static("src"));
+
+function TransformStream(mods) {
+	return stream.Transform({
+		writableObjectMode: true,
+		transform: mods
+	});
+}
+
+function pipeToMod(piper, url) {
+	let mod = new TransformStream(function(chunk, _, callback) {
+		//console.log(chunk);
+		/*let match, rehref = /href="[^"]+"/ig, resrc = /src="[^"]+"/ig;
+		let tsc = chunk.toString();
+		while ((match = rehref.exec(tsc)) != null) {
+			tsc = tsc.replace(rehref, "href='peroxyde.dexie.codes/?s=" + url +
+			split(tsc, match.index+4, match.index) + "'");
+			callback(null, chunk);
+		}
+		while ((match = resrc.exec(tsc)) != null) {
+			tsc = tsc.replace()
+		}*/
+		callback(null, chunk);
+	});
+	return piper.pipe(mod);
+}
+
+app.listen(port, () => {
+	console.log(`Proxy server running on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+	let bypass = req.query.s;
+	if (bypass) {
+		if (!(bypass.slice(0, 7) === "http://" || bypass.slice(0, 8) === "https://")) {
+			bypass = "https://" + bypass;
+			console.log("fixed big boi haxxor url");
+		}
+		pipeToMod(request(bypass), bypass).pipe(res); // Important
+		timesUsed++;
+	} else {
+		res.render("home", { uses: timesUsed, lastReset: lastReset });
+	}
+	//res.end();
+});
